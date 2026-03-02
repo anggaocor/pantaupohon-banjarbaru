3698b542850d83c354e1a3edfac15ad7af9ef784
anggaocor <anggaalrasyid.dlh@gmail.com>
2026-03-02 15:23:24 +0800

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,16 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
   },
+
   transpilePackages: ['react-leaflet'],
 
   typescript: {
