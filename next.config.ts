93c3912bb684556c9097203fde0d97cc8275e77c
anggaocor <anggaalrasyid.dlh@gmail.com>
2026-03-02 15:19:20 +0800

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
   },
   transpilePackages: ['react-leaflet'],
 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   typescript: {
     ignoreBuildErrors: true,
   },
